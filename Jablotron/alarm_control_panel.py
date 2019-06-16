33f682435123d47761b666467de32c4f5393f6b2
mattsaxon <saxonmatt@hotmail.com>
2019-06-16 22:05:28 +0100

    Desired state management
    Changed code to manage the system to a specific state to deal with failures and parallel execution.
    
    Also tidies up thread vs async code to improve responsiveness, reliability and simplicity.

--- Jablotron/alarm_control_panel.py
+++ Jablotron/alarm_control_panel.py
@@ -56,14 +56,24 @@ class JablotronAlarm(alarm.AlarmControlPanel):
         self._model = 'Unknown'
         self._lock = threading.BoundedSemaphore()
         self._stop = threading.Event()
+        self._updated = asyncio.Event()
+        self._desired_state_updated = asyncio.Event()
+        self._wait_task = None
 
         try:         
             hass.bus.async_listen('homeassistant_stop', self.shutdown_threads)
 
             from concurrent.futures import ThreadPoolExecutor
             self._io_pool_exc = ThreadPoolExecutor(max_workers=5)    
-            self._io_pool_exc.submit(self._startup_message)
+            #self._io_pool_exc.submit(self._startup_message)
+
+            self._startup_message()
+
             self._read_loop_future = self._io_pool_exc.submit(self._read_loop)
+
+            self.loop = asyncio.get_running_loop()
+
+            self.loop.create_task(self.send_command_loop())
 
         except Exception as ex:
             _LOGGER.error('Unexpected error: %s', format(ex) )
@@ -74,6 +84,8 @@ class JablotronAlarm(alarm.AlarmControlPanel):
         _LOGGER.debug('handle_shutdown() called' )
 
         self._stop.set()
+        if self._wait_task is not None:
+            self._wait_task.cancel()
 
         _LOGGER.debug('exiting handle_shutdown()' )
 
@@ -111,26 +123,19 @@ class JablotronAlarm(alarm.AlarmControlPanel):
             return alarm.FORMAT_NUMBER
         return alarm.FORMAT_TEXT
 
-    async def _update_loop(self):
-
-        while True:
-            await self._update_required.wait()
-            self.async_schedule_update_ha_state()
-            self._update_required.clear()
-
     async def _update(self):
 
-            self.async_schedule_update_ha_state()
+        self._updated.set()
+        self.async_schedule_update_ha_state()
 
     def _read_loop(self):
 
         try:
+            self._f = open(self._file_path, 'rb', 64)
 
             while not self._stop.is_set():
 
-                self._lock.acquire()
-
-                self._f = open(self._file_path, 'rb', 64)
+                #self._lock.acquire()
 
                 new_state = self._read()
 
@@ -140,9 +145,7 @@ class JablotronAlarm(alarm.AlarmControlPanel):
 
                     asyncio.run_coroutine_threadsafe(self._update(), self._hass.loop)                   
 
-                self._f.close()
-
-                self._lock.release()
+                #self._lock.release()
 
                 time.sleep(1) # read state once every second, no need for more!
 
@@ -150,6 +153,7 @@ class JablotronAlarm(alarm.AlarmControlPanel):
             _LOGGER.error('Unexpected error: %s', format(ex) )
 
         finally:
+            self._f.close()
             _LOGGER.debug('exiting read_loop()' )
 
     def _read(self):
@@ -184,10 +188,16 @@ class JablotronAlarm(alarm.AlarmControlPanel):
             b'\x8f': "Key Press"
         }
 
+        state = None
+        old_state = None        # maintain old state and new state
+        state_consistent_count = 0    # ensure state stays consistent for a number of times to week our spurious state changes
+
         try:
             while True:
 
+                self._lock.acquire()
                 packet = self._f.read(64)
+                self._lock.release()
 
                 if not packet:
                     _LOGGER.warn("No packets")
@@ -201,18 +211,26 @@ class JablotronAlarm(alarm.AlarmControlPanel):
                     self._model = 'Jablotron JA-80 Series'
                     byte_two = int.from_bytes(packet[1:2], byteorder='big', signed=False)
                     
-                    if byte_two >= 1 and byte_two <= 8: # all 2nd packets I have seen are between 1 and 7 
+                    if byte_two == 1 and byte_two <= 8: # and byte_two != 2: # all 2nd packets I have seen are between 1 and 8, but 2 packets sometimes have trigger message 
 
                         state = ja82codes.get(packet[2:3]) # the state is in the 3rd packet
 
                         if state is None:
-                            _LOGGER.debug("Unknown status packet is %s", packet[:8])
+                            # _LOGGER.debug("Unknown status packet is %s", packet[:8])
+                            pass
 
                         elif state != "Heartbeat?" and state !="Key Press" and state !="?" :
-                            return state
-
-                    else:
-                        _LOGGER.warn("Unknown packet is %s", packet[:8])
+                            # _LOGGER.debug("state: %s, packet %s", state, packet[:3])
+                            if state == old_state:
+                                state_consistent_count +=1
+                                if state_consistent_count >= 1:
+                                    return state
+                            else:
+                                state_consistent_count = 0
+                                old_state = state
+                                
+                    #else:
+                    #    _LOGGER.warn("Unknown packet is %s", packet[:8])
 
                 else:         
                     _LOGGER.error("The data stream is not recongisable as a JA-82 control panel. Please raise an issue at https://github.com/mattsaxon/HASS-Jablotron80/issues with this packet info [%s]", packet)
@@ -230,7 +248,6 @@ class JablotronAlarm(alarm.AlarmControlPanel):
             return 'Failed'
 
 
-
     async def async_alarm_disarm(self, code=None):
         """Send disarm command.
 
@@ -243,8 +260,10 @@ class JablotronAlarm(alarm.AlarmControlPanel):
                 code = self._code
             send_code = code
 
-        payload = "*0"
-        self._sendKeys(send_code, payload)
+        action = "*0"
+
+        await self._sendCommand(send_code, action, STATE_ALARM_DISARMED)
+
 
     async def async_alarm_arm_home(self, code=None):
         """Send arm home command.
@@ -256,7 +275,9 @@ class JablotronAlarm(alarm.AlarmControlPanel):
             send_code = code
 
         action = "*2"
-        self._sendKeys(send_code, action)
+
+        await self._sendCommand(send_code, action, STATE_ALARM_ARMED_HOME)
+
 
     async def async_alarm_arm_away(self, code=None):
         """Send arm away command.
@@ -268,7 +289,9 @@ class JablotronAlarm(alarm.AlarmControlPanel):
             send_code = code
 
         action = "*1"
-        self._sendKeys(send_code, action)
+
+        await self._sendCommand(send_code, action, STATE_ALARM_ARMED_AWAY)
+
 
     async def async_alarm_arm_night(self, code=None):
         """Send arm night command.
@@ -280,17 +303,76 @@ class JablotronAlarm(alarm.AlarmControlPanel):
             send_code = code
 
         action = "*3"
-        self._sendKeys(send_code, action)
 
-    def _sendKeys(self, code, action):
-        """Send via serial port."""
+        await self._sendCommand(send_code, action, STATE_ALARM_ARMED_NIGHT)
+
+
+    async def _sendCommand(self, code, action, desired_state):
+
         payload = action
-
-        _LOGGER.debug("sending %s", payload)
 
         if code is not None:
             payload += code
-        
+
+        self._payload = payload
+        self._desired_state = desired_state
+
+        self._desired_state_updated.set()
+
+        if self._wait_task is not None:
+            self._wait_task.cancel()
+
+
+    async def send_command_loop(self):
+
+        _LOGGER.debug('send_command_loop() enter')
+
+        while not self._stop.is_set():
+
+            retrying = False
+
+            await self._desired_state_updated.wait()
+            self._desired_state_updated.clear()
+
+            _LOGGER.debug('command received: %s', self._payload)
+
+            while self.state != self._desired_state:
+
+                self._updated.clear()
+
+                if not retrying or (self.state != STATE_ALARM_ARMING and self.state != STATE_ALARM_PENDING) :
+                    await self._send_keys(self._payload)
+
+                try:
+
+                    if self._desired_state == STATE_ALARM_DISARMED:
+                        timeout = 5
+                    else:
+                        timeout = 40
+
+                    self._wait_task = self.loop.create_task(self._updated.wait())
+                    await asyncio.wait_for(self._wait_task, timeout)
+                    self._updated.clear()
+
+                except asyncio.TimeoutError:
+                    _LOGGER.warn('Timed out waiting for change of state, retry')
+
+                except asyncio.CancelledError:
+                    _LOGGER.debug('New desired state set, wait has been cancelled, loop')
+                    break
+
+                retrying = True
+
+                _LOGGER.debug('state: %s', self.state)
+
+        _LOGGER.debug('send_command_loop(): exit')
+
+
+    async def _send_keys(self, payload):
+        """Send via serial port."""
+
+        _LOGGER.debug("sending %s", payload)
+       
         key_map = {
             "0": b'\x80',
             "1": b'\x81',
@@ -315,31 +397,26 @@ class JablotronAlarm(alarm.AlarmControlPanel):
                 packet_no +=1
                 packet = b'\x00\x02\x01' + key_map.get(c)
                 _LOGGER.debug('sending packet %i, message: %s', packet_no, packet)
-                self._sendPacket(packet)
+                self._send_packet(packet)
+                await asyncio.sleep(0.1) # lower reliability without this delay
 
         except Exception as ex:
-                    _LOGGER.error('Unexpected error: %s', format(ex) )
+            _LOGGER.error('Unexpected error: %s', format(ex) )
 
         finally:
             self._lock.release()
 
-
-    def _sendPacket(self, packet):
+    def _send_packet(self, packet):
         f = open(self._file_path, 'wb')
         f.write(packet)
-        time.sleep(0.1) # lower reliability without this delay
         f.close()        
+
+    # todo: send immediately
 
     def _startup_message(self):
         """ Send Start Message to panel"""
 
-        try:
-            # self._lock.acquire()
+        _LOGGER.debug('Sending startup message')
+        self._send_packet(b'\x00\x00\x01\x01')
+        _LOGGER.debug('Successfully sent startup message')
 
-            _LOGGER.debug('Sending startup message')
-            self._sendPacket(b'\x00\x00\x01\x01')
-            _LOGGER.debug('Successfully sent startup message')
-
-        finally:
-            pass
-            #self._lock.release()
